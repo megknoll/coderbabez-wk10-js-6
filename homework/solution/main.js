e04c25f3210a7b5d401c00998364540525899326
megknoll <megknoll@gmail.com>
2017-08-15 19:36:40 -0400

    adding new images

--- homework/solution/main.js
+++ homework/solution/main.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
-	// When the button is clicked, get the value from the text field, and call getWeather with the text as the argument. 
+	// When the button is clicked, get the value from the text field, and call getWeather with the text as the argument.
 	$("button").click(function(){
 		var city = $("input").val();
-		var weather = getWeather(city);
+		console.log(city);
+		getWeather(city);
 	});
 });
 
 // Write a function called displayWeather that takes one parameter, weather.
 // Use an if statement to display the appropriate image from the img folder based on the value of weather.
 // If there is no match, display the na image.
-// Possible Weather values are: Thunderstorm, Drizzle, Rain, Snow, Clouds, Clear, Extreme
+// Possible Weather values are: Thunderstorm, Drizzle, Rain, Mist, Snow, Clouds, Clear, Extreme
 
 function displayWeather(weather){
 	$("#weatherText").html(weather);
 	if(weather === "Mist"){
 		$("#weatherImg").attr("src","./img/rain.png");
-	} else {
+	} else if(weather === "Rain"){
+		$("#weatherImg").attr("src","./img/rain.png");
+	}else {
 		$("#weatherImg").attr("src","./img/na.png");
 	}
 }
